7678442ec448fdda728d209a5ae83296926b47ff
Kyle May <kyle.pat.may@gmail.com>
2023-01-02 22:03:35 -0800

    init code for cmake proj

---
+++ instrumentation/instrumentation.cpp
@@ -0,0 +1,12 @@
+﻿// instrumentation.cpp : Defines the entry point for the application.
+//
+
+#include "instrumentation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
